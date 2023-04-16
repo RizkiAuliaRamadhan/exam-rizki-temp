c1caa689ccaee57893494d7b46353225a9631617
Rizki Aulia Ramadhan <rwamdhaney2@gmail.com>
2023-04-16 09:28:16 +0700

    update

---
+++ fixKecoa/copy.js
@@ -0,0 +1,7 @@
+const fs = require("fs")
+
+fs.copyFile("fixKecoa/index.js", "node_modules/knex/lib/dialects/postgres/index.js", err => {
+    if(err) {
+        console.log("Error Found:", err);
+    }
+})
